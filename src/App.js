7038309cee78c3a5ffde095467da3f2dd1660a36
Fr1k1 <martin.friscic6@gmail.com>
2022-08-05 20:07:20 +0200

    before adding plans

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./index.css";
 import "./App.css";
 import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/Programs";
+import Reasons from "./Components/Reasons/Reasons";
 
 function App() {
   return (
     <div className="App">
       <Hero />
-      <Programs/>
+      <Programs />
+      <Reasons />
     </div>
   );
 }
