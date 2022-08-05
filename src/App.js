c635f33d0ed9b593882e9cfa0b60f4709adf5980
Fr1k1 <martin.friscic6@gmail.com>
2022-08-05 16:47:56 +0200

    Mapped thru programs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./index.css";
-import "./App.css"
+import "./App.css";
 import Hero from "./Components/Hero/Hero";
+import Programs from "./Components/Programs/Programs";
 
 function App() {
-  return (<div className="App">
-    <Hero/>
-  </div>);
+  return (
+    <div className="App">
+      <Hero />
+      <Programs/>
+    </div>
+  );
 }
 
 export default App;
