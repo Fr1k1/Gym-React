4116ea343dc9c3d0b8106fd4e4cf8cf566b3a047
Fr1k1 <martin.friscic6@gmail.com>
2022-08-13 15:45:48 +0200

    Added mail sending

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Programs from "./Components/Programs/Programs";
 import Reasons from "./Components/Reasons/Reasons";
 import Plans from "./Components/Plans/Plans";
 import Testimonials from "./Components/Testimonials/Testimonials";
+import JoinUs from "./Components/JoinUs/JoinUs";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Reasons />
       <Plans/>
       <Testimonials/>
+      <JoinUs/>
     </div>
   );
 }
---
+++ src/Components/JoinUs/JoinUs.jsx
@@ -0,0 +1,55 @@
+import React, { useRef } from "react";
+import "./JoinUs.css";
+import emailjs from '@emailjs/browser';
+
+const JoinUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_hdkrryu",
+        "template_9de3j0r",
+        form.current,
+        "Z_0-Cs6CmcZEESvvj"
+      )
+      .then(
+        (result) => {
+          alert("Uspjesno poslano");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div className="Join" id="join-us">
+      <div className="left-j">
+        <hr />
+        <div>
+          <span class="stroke-text">READY TO</span>
+          <span>LEVEL UP</span>
+        </div>
+        <div>
+          <span>YOUR BODY</span>
+          <span class="stroke-text">WITH US?</span>
+        </div>
+      </div>
+      <div className="right-j">
+        <form
+          ref={form}
+          action=""
+          className="email-container"
+          onSubmit={sendEmail}
+        >
+          <input type="email" name="user_email" placeholder="Unesite EMAIL" />
+          <button className="btn btn-j">Join now</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default JoinUs;
